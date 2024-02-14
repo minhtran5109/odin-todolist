4c2ac55fd49aacf24a266315d7218466b9fe052a
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-02-15 10:49:53 +1100

    added webpack config for css file and modal styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
+import './style.css';
 
-const container = document.querySelector("#container")
+const projects = [];
+
+const container = document.querySelector("#container");
+const addProjectBtn = document.getElementById("add-project");
+const projectModal = document.getElementById("project-modal");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority
+    }
+}
+
+class Project {
+    constructor(title) {
+        this.title = title;
+    }
+}
+
 function render() {
-    container.innerHTML = `<p>HELLO</p>`;
+    // container.innerHTML = `<p>HELLO</p>`;
+    addProjectBtn.addEventListener('click', () => {
+        projectModal.style.display = "block";
+    });
 
+    closeBtn.addEventListener('click', () => {
+        projectModal.style.display = "none";
+    })
 }
 
 render();
