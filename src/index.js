889c24f196abcb799080fc09e5f6276a362089bd
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-02-15 11:17:10 +1100

    implemented adding project function and display

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const container = document.querySelector("#container");
 const addProjectBtn = document.getElementById("add-project");
 const projectModal = document.getElementById("project-modal");
 const closeBtn = document.getElementsByClassName("close")[0];
+const submitProject = document.getElementById("submit-project");
 
 class TodoItem {
     constructor(title, description, dueDate, priority) {
@@ -22,15 +23,44 @@ class Project {
     }
 }
 
+function addProject() {
+    let projectTitle = document.getElementById("project-title").value;
+    let newProject = new Project(projectTitle);
+    projects.push(newProject);
+}
+
 function render() {
     // container.innerHTML = `<p>HELLO</p>`;
-    addProjectBtn.addEventListener('click', () => {
-        projectModal.style.display = "block";
-    });
+    projects.forEach((project, index) => {
+        let card = document.createElement('div');
+        card.classList.add("card");
 
-    closeBtn.addEventListener('click', () => {
-        projectModal.style.display = "none";
+        let title = document.createTextNode(project.title);
+        card.appendChild(title);
+        card.appendChild(document.createElement('br'));
+
+        let addTodoBtn = document.createElement('button');
+        addTodoBtn.textContent = "Add Task";
+        card.appendChild(addTodoBtn);
+
+        container.appendChild(card);
     })
 }
+
+addProjectBtn.addEventListener('click', () => {
+    projectModal.style.display = "block";
+});
+
+closeBtn.addEventListener('click', () => {
+    projectModal.style.display = "none";
+})
+
+submitProject.addEventListener('click', (e) => {
+    e.preventDefault();
+    addProject();
+    console.log(projects);
+    projectModal.style.display = "none";
+    render();
+})
 
 render();
